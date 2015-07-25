99f2d1b068c185769a9e2734abaa431140a0ab25
mcetraro <marco.cetraro@gmail.com>
2015-07-25 19:20:12 -0400

    Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+# To obtain the Matrix inversion, the programmer would have to create several lines 
+# code to calculate it. The following two functions are used to cache the inverse of 
+# a matrix.
 
-## Write a short comment describing this function
+# makeCacheMatrix creates a list containing a function to:
+# 1. set the value of the matrix.
+# 2. get the value of the matrix.
+# 3. set the value of inverse of the matrix.
+# 4. get the value of inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+# The next function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If so, it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache via
+# setinverse function.
 
+# This function assumes that the matrix is always invertible.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+
+# This is an example:
+# x <- cbind(c(1,3), c(5,6))
+> x
+#       [,1] [,2]
+# [1,]    1    5
+# [2,]    3    6
+# m = makeCacheMatrix(x)
+# m$get()
+#       [,1] [,2]
+# [1,]    1    5
+# [2,]    3    6
+# cacheSolve(m)
+#       [,1]       [,2]
+# [1,] -0.6666667  0.5555556
+# [2,]  0.3333333 -0.1111111
+# zz <- cacheSolve(m)
+# zz1 <- m$get()
+# zz%*%zz1
+#       [,1] [,2]
+# [1,]    1    0
+# [2,]    0    1
